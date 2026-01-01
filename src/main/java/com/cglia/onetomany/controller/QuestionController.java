433de209b3d15c577dc228b5dc60414e5de0e755
Rahul Gupta <rahuldevit2000@gmail.com>
2026-01-01 21:11:22 +0530

    Update QuestionController.java
    Addling Logs In Controller For Getting the better Logs to find the Issue.

--- src/main/java/com/cglia/onetomany/controller/QuestionController.java
+++ src/main/java/com/cglia/onetomany/controller/QuestionController.java
@@ -5,12 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import com.cglia.onetomany.entity.Question;
 import com.cglia.onetomany.service.QuestionService;
-
 import jakarta.transaction.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -92,3 +89,4 @@ public class QuestionController {
         logger.info("Question deleted successfully for id {}", id);
     }
 }
+
