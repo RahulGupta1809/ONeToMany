6c6dc1656f8d0c4480b0384f0d2de5efcb199e72
Rahul Gupta <rahuldevit2000@gmail.com>
2026-01-31 13:51:35 +0530

    Update AnswerRepository.java
    Answer Change

--- src/main/java/com/cglia/onetomany/repository/AnswerRepository.java
+++ src/main/java/com/cglia/onetomany/repository/AnswerRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cglia.onetomany.entity.Answer;
-
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
 }
+
